3454ad25565db93b291e420c25a72c5d76e0be0e
sriramgandrothu <sriramgandorthu@gmail.com>
2024-07-22 20:49:26 +0530

    Django-3

--- emailverpro/emailverpro/settings.py
+++ emailverpro/emailverpro/settings.py
@@ -113,8 +113,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''  # Replace with your Gmail address
-EMAIL_HOST_PASSWORD = ''  # Replace with your Gmail app-specific password or regular password if less secure apps are enabled
+EMAIL_HOST_USER = 'sriramgandrothu@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = 'ynowpopvbdmjgbcw'  # Replace with your Gmail app-specific password or regular password if less secure apps are enabled
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
